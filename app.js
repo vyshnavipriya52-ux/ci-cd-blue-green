40a45e2b058d36c25148df89e9d2ee5f3646c314
EC2 Default User <ec2-user@ip-172-31-41-31.ap-south-1.compute.internal>
2026-03-25 09:22:20 +0000

    updated html,css and app.js files

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// serve HTML + CSS
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000);
-//trigger pipeline
-//trigger after secrets
-//trigger after secrets
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
